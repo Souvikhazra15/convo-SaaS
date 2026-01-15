dfb9f4d1819f6a12a615e7e4e6283aceb7914dac
Souvik Hazra <souvikhazra1901@gmail.com>
2026-01-16 01:33:13 +0530

    auth-setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import Navitems from './Navitems'
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Navitems from "@/components/Navitems";
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
-            <Link href='/'>
-                <div className='flex items-center gap-2.5 cursor-pointer'>
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image
                         src="/images/logo.svg"
-                        alt="Logo"
+                        alt="logo"
                         width={46}
-                        height={44} />
+                        height={44}
+                    />
                 </div>
             </Link>
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <Navitems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
